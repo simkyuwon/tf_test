12d254400c5380fbde82846f9a042a30f625ef6c
sim <tlarbdnjs999@gmail.com>
2021-09-28 00:03:16 +0900

    temp

--- make_model.py
+++ make_model.py
@@ -7,44 +7,72 @@ data_dir = pathlib.Path("./data/TRAIN_DIR")
 
 
 def make_model():
-    train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-        data_dir,
-        validation_split=0.2,
-        batch_size=64,
-        subset="training",
-        seed=random.randrange(1, 100000),
-        shuffle=10000,
-        image_size=(64, 88))
+    train_data_generator = tf.keras.preprocessing.image.ImageDataGenerator(
+        rotation_range=10,
+        brightness_range=[0.8, 1.0],
+        shear_range=5,
+        fill_mode='constant',
+        cval=0
+    )
 
-    class_names = train_ds.class_names
+    validation_data_generator = tf.keras.preprocessing.image.ImageDataGenerator()
 
-    val_ds = tf.keras.preprocessing.image_dataset_from_directory(
-        data_dir,
-        validation_split=0.2,
+    train_ds = train_data_generator.flow_from_directory(
+        f'{data_dir}/TRAIN',
+        target_size=(240, 320),
         batch_size=64,
-        subset="validation",
-        seed=random.randrange(1, 100000),
-        image_size=(64, 88))
+        color_mode='grayscale',
+        seed=random.randrange(1, 1000)
+    )
+
+    val_ds = validation_data_generator.flow_from_directory(
+        f'{data_dir}/VALIDATION',
+        target_size=(240, 320),
+        batch_size=64,
+        color_mode='grayscale',
+        seed=random.randrange(1, 1000)
+    )
+
+    # train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    #     data_dir,
+    #     validation_split=0.2,
+    #     batch_size=64,
+    #     subset="training",
+    #     color_mode='grayscale',
+    #     seed=random.randrange(1, 100000),
+    #     shuffle=10000,
+    #     image_size=(240, 320))
+
+    # val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    #     data_dir,
+    #     validation_split=0.2,
+    #     batch_size=64,
+    #     subset="validation",
+    #     color_mode='grayscale',
+    #     seed=random.randrange(1, 100000),
+    #     image_size=(240, 320))
 
     model = tf.keras.Sequential([
-        layers.Resizing(64, 88),
+        layers.Resizing(240, 320),
         layers.Rescaling(1. / 255),
-        layers.Conv2D(30, (3, 3), dilation_rate=(2, 2), activation='relu'),
+        layers.Conv2D(30, (11, 11), activation='relu'),
         layers.MaxPool2D(),
-        layers.Conv2D(30, (3, 3), activation='relu'),
+        layers.Conv2D(30, (9, 9), activation='relu'),
         layers.MaxPool2D(),
-        layers.Conv2D(30, (3, 3), activation='relu'),
+        layers.Conv2D(40, (7, 7), activation='relu'),
         layers.MaxPool2D(),
-        layers.Conv2D(30, (3, 3), activation='relu'),
+        layers.Conv2D(40, (5, 5), activation='relu'),
+        layers.MaxPool2D(),
+        layers.Conv2D(50, (3, 3), activation='relu'),
         layers.MaxPool2D(),
         layers.Flatten(),
-        layers.Dense(100, activation='relu'),
-        layers.Dense(5, activation='softmax')
+        layers.Dense(120, activation='relu'),
+        layers.Dense(6, activation='softmax')
     ])
 
     model.compile(
         optimizer="adam",
-        loss='sparse_categorical_crossentropy',
+        loss=tf.keras.losses.CategoricalCrossentropy(),
         metrics=["accuracy"]
     )
 
@@ -54,20 +82,20 @@ def make_model():
     model.fit(
         train_ds,
         validation_data=val_ds,
-        epochs=30,
+        epochs=20,
         # callbacks=[tensorboard_callback]
     )
 
     model.summary()
 
     model.save('saved_model/model')
-    # converter = tf.lite.TFLiteConverter.from_saved_model("saved_model/model")
-    # converter.optimizations = [tf.lite.Optimize.DEFAULT]
-    # converter.target_spec.supported_types = [tf.float16]
-    # tflite_model = converter.convert()
-    #
-    # model_file = pathlib.Path('model.tflite')
-    # model_file.write_bytes(tflite_model)
+    converter = tf.lite.TFLiteConverter.from_saved_model("saved_model/model")
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+    converter.target_spec.supported_types = [tf.float16]
+    tflite_model = converter.convert()
+
+    model_file = pathlib.Path('model.tflite')
+    model_file.write_bytes(tflite_model)
 
 
 if __name__ == "__main__":
